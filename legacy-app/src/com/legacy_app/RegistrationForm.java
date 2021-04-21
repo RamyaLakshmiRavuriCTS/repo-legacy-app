a786c2f69fcc4f7578ff130e06e3d84cec2f276d
eltjavatraining <eltjavatraining>
2021-04-21 11:53:24 +0530

    Week4 assignment

---
+++ legacy-app/src/com/legacy_app/RegistrationForm.java
@@ -0,0 +1,72 @@
+package com.legacy_app;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentServlet
+ */
+@WebServlet("/RegistrationForm")
+public class RegistrationForm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistrationForm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");  
+		PrintWriter out = response.getWriter();  
+		out.println("<html><body>");
+		
+		out.write("<html>"+
+		          "<body>"+
+				 "<table border='1'>"+
+		          "<tr>"+
+				 "<td>First Name</td>"+
+		          "<td>"+request.getParameter("firstName")+"</td>"+
+		          "</tr>"+
+		          "<tr>"+
+				 "<td>Last Name</td>"+
+			      "<td>"+request.getParameter("lastName")+"</td>"+
+				  "</tr>"+
+				  "<tr>"+
+				 "<td>Address</td>"+
+				  "<td>"+request.getParameter("address")+"</td>"+
+				 "</tr>"+
+				 "<tr>"+
+				 "<td>Country</td>"+
+			      "<td>"+request.getParameter("country")+"</td>"+
+				  "</tr>"
+				);
+		out.write("</table>"+
+				"</body>"+
+				"</html>");
+		
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
